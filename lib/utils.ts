37ac9d6ef8cc64680df643d5c057a118c8c27d5c
Ak <3677066+AkashKrMasih@users.noreply.github.com>
2026-09-25 09:01:19 +0530

    add colored console log function

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+const colors = {
+  reset: "\x1b[0m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  gray: "\x1b[90m",
+};
+
+export function log(color: keyof typeof colors, msg: string) {
+  console.log(`${colors[color]}${msg}${colors.reset}`);
+}
